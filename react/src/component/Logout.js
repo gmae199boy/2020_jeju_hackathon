98e84ae5a4df8403ee39560b7081c98c4747ab0e
gmae199boy <kim88594544@gmail.com>
2020-10-16 20:40:30 +0900

    commit

--- react/src/component/Logout.js
+++ react/src/component/Logout.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { withRouter } from 'react-router-dom';
 
 function LogoutButton({ logout, history }) {
+  const [logout, setLogout] = useState(null);
   const handleClick = () => {
-    logout();
-    history.push('/');
+    window.localStorage.removeItem('user');
+
+    // logout();
+    // history.push('/');
   }
-  return <button onClick={handleClick}>Logout</button>;
+  return (
+    <div>
+      <button onClick={handleClick}>Logout</button>
+      {logout && <Redirect to="/login" /> }
+    </div>
+  );
 }
 
 export default withRouter(LogoutButton);
